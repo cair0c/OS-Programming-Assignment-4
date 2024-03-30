7f5cda3562f345afc960dbec1a9702c963e142c5
cair0c <cairocraw@gmail.com>
2024-03-29 21:38:22 -0400

    adding shared counter, various print statements, preprocessor macros, changed input for testing purposes

--- main.c
+++ main.c
@@ -11,17 +11,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_READERS 12
+#define NUM_READS 2000000
+#define NUM_WRITES 25000
+
+volatile int sharedCounter = 0;
+
 int main(int argc, char *argv[]) {
     // checking for the correct amount of arguments in input
+    int numReaders;
+
+    printf("Enter number of readers: \n");
+    scanf("%d", &numReaders);
+
+    /*
     if (argc != 2) {
         printf("Usage: %s <number_of_readers>\n", argv[0]);
         return 1;
     }
+    */
 
     // checking if number of readers is in the correct range
-    int num_readers = atoi(argv[1]);
-    if (num_readers <= 0 || num_readers > 12) {
-        printf("Number of readers must be between 1 and 12 inclusive.\n");
+    //int numReaders = atoi(argv[1]);
+    if (numReaders < 0 || numReaders > MAX_READERS) {
+        printf("Number of readers must be between 1 and 12.\n");
         return 1;
+    }
+
+    printf("Executing... please wait...\n");
+
+    for (int count = 0; count < 25000; count++) {
+        sharedCounter++;
+    }
+
+    printf("Writer Done!\n");
+
+    for (int reader = 0; reader < numReaders; reader++) {
+        printf("I'm reader%d, counter = %d\n", reader + 1, sharedCounter);
     }
 }
