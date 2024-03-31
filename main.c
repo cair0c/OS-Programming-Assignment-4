a9e2f65a8860b9cd33a7f00e9e143b71624e2c17
the-beef-calculator <77137812+the-beef-calculator@users.noreply.github.com>
2024-03-30 23:08:07 -0400

    Update main.c

--- main.c
+++ main.c
@@ -13,45 +13,156 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-
 #define MAX_READERS 12
 #define NUM_READS 2000000
 #define NUM_WRITES 25000
 
 volatile int sharedCounter = 0;
-sem_t mutex;
-sem_t wrt;
+sem_t mutex, wrt;
+int readCount = 0;
 
-int main(int argc, char *argv[]) {
-    // checking for the correct amount of arguments in input
-    int numReaders;
+void *reader(void *param);
+void *writer(void *param);
 
-    printf("Enter number of readers: \n");
-    scanf("%d", &numReaders);
+int main(int argc, char *argv[])
+{
 
-    /*
-    if (argc != 2) {
-        printf("Usage: %s <number_of_readers>\n", argv[0]);
+   //performing input validation
+   if (argc != 2)
+   {
+        printf("Usage: %s <number of readers>\n", argv[0]);
         return 1;
     }
-    */
 
-    // checking if number of readers is in the correct range
-    //int numReaders = atoi(argv[1]);
-    if (numReaders < 0 || numReaders > MAX_READERS) {
+    int numReaders = atoi(argv[1]);
+    if (numReaders < 1 || numReaders > MAX_READERS)
+    {
         printf("Number of readers must be between 1 and 12.\n");
         return 1;
     }
 
+
+    // Initializing sempahores
+    sem_init(&mutex, 0, 1);
+    sem_init(&wrt, 0, 1);
+
+    pthread_t readers[numReaders], writer_thread;
+    int readerNums[numReaders];
+
     printf("Executing... please wait...\n");
 
-    for (int count = 0; count < 25000; count++) {
+    // Create the writer thread
+    pthread_create(&writer_thread, NULL, writer, NULL);
+
+    // Create reader threads
+    for (int i = 0; i < numReaders; i++)
+    {
+        readerNums[i] = i + 1;
+        pthread_create(&readers[i], NULL, reader, &readerNums[i]);
+    }
+
+    // Wait for the writer to finish
+    pthread_join(writer_thread, NULL);
+
+    // Wait for all readers to finish
+    for (int i = 0; i < numReaders; i++)
+    {
+        pthread_join(readers[i], NULL);
+    }
+
+    return 0;
+}
+
+// The reader function that will be executed by each reader thread.
+void *reader(void *param)
+{
+    // pull reader number from parameter
+    int readerNum = *((int *)param);
+
+    for (int i = 0; i < NUM_READS; i++)
+    {
+        // Wait to acquire the mutex before modifying readCount.
+        // if there's an error, exit the program
+        if (sem_wait(&mutex) != 0)
+        {
+            perror("sem_wait (mutex) in reader failed");
+            exit(1);
+        }
+
+        readCount++;
+
+        // Checks if this is the first reader. If so, it must wait. prints error
+        // code if it fails
+        if (readCount == 1)
+        {
+            if (sem_wait(&wrt) != 0)
+            {
+                perror("sem_wait (wrt) in reader failed");
+                exit(1);
+            }
+        }
+
+        // Release the mutex to allow other readers to increment the readCount
+        if (sem_post(&mutex) != 0)
+        {
+            perror("sem_post (mutex) in reader failed");
+            exit(1);
+        }
+
+        // Critical section (reading)
+
+        if (sem_wait(&mutex) != 0)
+        {
+            perror("sem_wait (mutex) in reader failed");
+            exit(1);
+        }
+
+        // to show this reader has finished reading
+        readCount--;
+
+        // checks if its the last reader, if so, it must release semaphore
+        if (readCount == 0)
+        {
+            if (sem_post(&wrt) != 0)
+            {
+                perror("sem_post (wrt) in reader failed");
+                exit(1);
+            }
+        }
+        if (sem_post(&mutex) != 0)
+        {
+            perror("sem_post (mutex) in reader failed");
+            exit(1);
+        }
+    }
+
+    printf("I'm reader%d, counter = %d\n", readerNum, sharedCounter);
+    return;
+}
+
+//writer function executed by the writer thread
+void *writer(void *param) {
+    for (int i = 0; i < NUM_WRITES; i++)
+    {
+        // Wait to acquire the write semaphore (wrt) before proceeding
+        // print an error and exit if theres a failure
+        if (sem_wait(&wrt) != 0)
+        {
+            perror("sem_wait (wrt) in writer failed");
+            exit(1);
+        }
+
+        // Critical section starts
         sharedCounter++;
+
+        // print an error and exit if theres a failure
+        if (sem_post(&wrt) != 0)
+        {
+            perror("sem_post (wrt) in writer failed");
+            exit(1);
+        }
     }
 
     printf("Writer Done!\n");
-
-    for (int reader = 0; reader < numReaders; reader++) {
-        printf("I'm reader%d, counter = %d\n", reader + 1, sharedCounter);
-    }
+    return;
 }
